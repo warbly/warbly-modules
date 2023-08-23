6f847bee95a2d10786521141179978c6aac93ef4
ryantoddgarza <ryantoddgarza@gmail.com>
2023-08-23 14:34:29 -0500

    refactor: convert SimpleGain to JS class syntax

--- src/SimpleGain.js
+++ src/SimpleGain.js
@@ -1,12 +1,51 @@
-function SimpleGain(context, opts = { connect: context.destination }) {
-  const gainNode = context.createGain();
-  gainNode.connect(opts.connect);
+class SimpleGain {
+  #context;
 
-  if (opts.gain && typeof opts.gain === 'number') {
-    gainNode.gain.value = opts.gain;
+  #input;
+
+  #output;
+
+  #gainValue;
+
+  constructor(context, opts = {}) {
+    this.#context = context;
+    this.#gainValue = opts.gain || 1;
+
+    const gainNode = context.createGain();
+    this.#input = gainNode;
+    this.#output = gainNode;
+
+    this.#setGain();
   }
 
-  return gainNode;
+  #setGain() {
+    this.#output.gain.value = this.#gainValue;
+  }
+
+  get input() {
+    return this.#input;
+  }
+
+  get output() {
+    return this.#output;
+  }
+
+  get gain() {
+    return this.#gainValue;
+  }
+
+  set gain(value) {
+    this.#gainValue = value;
+    this.#setGain();
+  }
+
+  connect(destination) {
+    this.#output.connect(destination.input || destination);
+  }
+
+  disconnect() {
+    this.#output.disconnect();
+  }
 }
 
 export default SimpleGain;
