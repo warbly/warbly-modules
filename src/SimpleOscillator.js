6288a9b85e8639e728b3ddf9f6af4b057405d200
ryantoddgarza <ryantoddgarza@gmail.com>
2023-11-01 10:30:55 -0500

    Convert `SimpleOscillator` to JS class syntax

--- src/SimpleOscillator.js
+++ src/SimpleOscillator.js
@@ -1,20 +1,86 @@
-function SimpleOscillator(context, opts = { connect: context.destination }) {
-  const oscNode = context.createOscillator();
-  oscNode.connect(opts.connect);
+class SimpleOscillator {
+  #context;
 
-  if (opts.frequency && typeof opts.frequency === 'number') {
-    oscNode.frequency.value = opts.frequency;
+  #output;
+
+  #detune;
+
+  #frequency;
+
+  #type;
+
+  constructor(context, opts = {}) {
+    this.#context = context;
+    this.#detune = opts.detune || 0;
+    this.#frequency = opts.frequency || 440;
+    this.#type = opts.type || 'sine';
+
+    const oscillator = context.createOscillator();
+    this.#output = oscillator;
+
+    this.#setDetune();
+    this.#setFrequency();
+    this.#setType();
   }
 
-  if (opts.detune && typeof opts.detune === 'number') {
-    oscNode.detune.value = opts.detune;
+  #setDetune() {
+    this.#output.detune.value = this.#detune;
   }
 
-  if (opts.type && typeof opts.type === 'string') {
-    oscNode.type = opts.type;
+  #setFrequency() {
+    this.#output.frequency.value = this.#frequency;
   }
 
-  return oscNode;
+  #setType() {
+    this.#output.type = this.#type;
+  }
+
+  get output() {
+    return this.#output;
+  }
+
+  get detune() {
+    return this.#detune;
+  }
+
+  set detune(value) {
+    this.#detune = value;
+    this.#setDetune();
+  }
+
+  get frequency() {
+    return this.#frequency;
+  }
+
+  set frequency(value) {
+    this.#frequency = value;
+    this.#setFrequency();
+  }
+
+  get type() {
+    return this.#type;
+  }
+
+  set type(value) {
+    this.#type = value;
+    this.#setType();
+  }
+
+  connect(destination) {
+    this.#output.connect(destination.input || destination);
+  }
+
+  disconnect() {
+    this.#output.disconnect();
+  }
+
+  start() {
+    this.#output.start();
+  }
+
+  stop() {
+    this.#output.stop();
+  }
 }
 
 export default SimpleOscillator;
